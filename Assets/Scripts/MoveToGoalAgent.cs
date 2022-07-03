cb687e422a423739f4822ac55bed0f50f85ce09d
SharodeCode <jacob.mcintyre@sharode.com>
2022-07-03 23:07:27 +1000

    Added: Initial files

---
+++ Assets/Scripts/MoveToGoalAgent.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class MoveToGoalAgent : Agent
+{
+    [SerializeField] private Transform targetTransform;
+    [SerializeField] private Material winMaterial;
+    [SerializeField] private Material losematerial;
+    [SerializeField] private MeshRenderer floorMeshRenderer;
+
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = Vector3.zero;
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(targetTransform.localPosition);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float moveX = actions.ContinuousActions[0];
+        float moveZ = actions.ContinuousActions[1];
+
+        float moveSpeed = 5f;
+        transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continousActions = actionsOut.ContinuousActions;
+        continousActions[0] = Input.GetAxisRaw("Horizontal");
+        continousActions[1] = Input.GetAxisRaw("Vertical");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Finish")
+        {
+            SetReward(1f);
+            EndEpisode();
+
+            floorMeshRenderer.material = winMaterial;
+        }
+
+        if (other.tag == "wall")
+        {
+            SetReward(-1f);
+            EndEpisode();
+
+            floorMeshRenderer.material = losematerial;
+        }
+    }
+}
